43525ef40207e4fab5009b946e1d086f190eb065
Benjamin Casalino <benjamincasalino@Benjamins-MacBook-Pro.local>
2015-10-02 14:21:34 -0700

    hmmm

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var nicksFlix = angular.module('nicksFlix', ['ui.router']);
 
-
-
 nicksFlix.config(function($stateProvider, $urlRouterProvider) {
     // home page w// header and body partials
   $stateProvider.state('home', {
